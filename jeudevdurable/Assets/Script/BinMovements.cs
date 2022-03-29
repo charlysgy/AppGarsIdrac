f46a4a18733e18d5d54e33c35256667ee37b5cbc
R-Onne <lesech.erwann@gmail.com>
2022-03-29 17:33:58 +0200

    build<Unity>: game1 - Add bin.png + mvmtScript

---
+++ jeudevdurable/Assets/Script/BinMovements.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinMovements : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    private float screenWidth = Screen.width;
+    private float moveSpeed = 300;
+
+    private void Update()
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        int i = 0;
+        while (i < Input.touchCount)
+        {
+            // move right
+            if (Input.GetTouch(i).position.x > screenWidth / 2)
+            {
+                MoveCharacter(1.0f);
+            }
+            // move left
+            else if (Input.GetTouch(i).position.x < screenWidth / 2)
+            {
+                MoveCharacter(-1.0f);
+            }
+            i++;
+        }
+        if (screenPos.x < 50)
+        {
+            transform.position = new Vector3(-2, 1, 0);
+            rb.velocity = Vector3.zero;
+        }
+
+        else if (screenPos.x > screenWidth - 50)
+        {
+            transform.position = new Vector3(2, 1, 0);
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    private void MoveCharacter(float input)
+    {
+        rb.AddForce(new Vector2(input * moveSpeed * Time.deltaTime, 0));
+    }
+
+    // Quand un object entre dans le boxCollider de la poubelle
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "recyclable")
+        {
+            // incrémente compteur de 1 pt
+        }
+        else
+        {
+            // perdre une vie ou faire descendre de x sec le compteur
+        }
+    }
+}
